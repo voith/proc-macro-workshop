23be774e57a1e10355053bce880cfdbc1888c827
voith <voithjm1@gmail.com>
2025-11-05 14:08:32 +0530

    testcase 4,5 & 12 are passing for bitfield

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,40 +1,87 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Ident, ItemStruct};
 
 #[proc_macro_attribute]
 pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as ItemStruct);
     let vis = &input.vis;
     let name = &input.ident;
-
-    let data_struct = match &input.data {
-        syn::Data::Struct(s) => s,
-        _ => {
-            return syn::Error::new_spanned(&input, "Expected a struct")
-                .to_compile_error()
-                .into()
-        }
-    };
 
     // Build a const expression that sums all field Specifier::BITS.
     let zero = 0usize;
     let mut total_bits = quote! { #zero };
-    if let syn::Fields::Named(fields) = &data_struct.fields {
+    let mut getters_setters = Vec::new();
+    let mut bit_offset = quote! { #zero };
+    if let syn::Fields::Named(fields) = &input.fields {
         for field in &fields.named {
             if let syn::Type::Path(tp) = &field.ty {
                 let ty_path = &tp.path;
                 // Rely on trait checking by referencing the associated const.
                 // If the type does not implement bitfield::Specifier, this will
                 // cause a compile error at the use site.
+                let field_bits = quote! {<#ty_path as ::bitfield::Specifier>::BITS};
                 total_bits = quote! {
-                    #total_bits + <#ty_path as ::bitfield::Specifier>::BITS
+                    #total_bits + #field_bits
                 };
+                let field_type = quote! {
+                    <#ty_path as ::bitfield::Specifier>::Ty
+                };
+                let ident = field.ident.as_ref().unwrap();
+                let getter = Ident::new(&format!("get_{}", ident), Span::call_site());
+                let setter = Ident::new(&format!("set_{}", ident), Span::call_site());
+
+                getters_setters.push(quote! {
+                    fn #getter(&self) -> #field_type {
+                        let mut bits_placed: usize = #bit_offset as usize; // total bits placed so far
+                        let mut remaining: u8 = #field_bits as u8;
+                        let mut value: u64 = 0b0;
+                        let mut previous_bits: u64 = 0; // data bits already collected 
+                        while remaining > 0 {
+                            let byte = bits_placed / 8;
+                            let start = (bits_placed % 8) as u8;
+                            let vacancy = 8 - start;
+                            let take = remaining.min(vacancy);
+                            let end = start + take - 1;
+
+                            value |= ((((self.data[byte] & create_get_bit_mask(start, end)) as u64) >> start)<< previous_bits) as u64;
+                            previous_bits += (end - start + 1) as u64;
+
+                            bits_placed += take as usize;
+                            remaining -= take;
+                        }
+                        value as #field_type
+                    }
+
+                     fn #setter(&mut self, value: #field_type) {
+                        let value = value as u64;
+                        let mut bits_placed: usize = #bit_offset as usize; // total bits placed so far
+                        let mut remaining: u8 = #field_bits as u8;
+                        let mut num_bits_set: u64 = 0;
+                        while remaining > 0 {
+                            let byte = bits_placed / 8;
+                            let start = (bits_placed % 8) as u8;
+                            let vacancy = 8 - start;
+                            let take = remaining.min(vacancy);
+                            let end = start + take - 1;
+
+                            self.data[byte] &= !create_get_bit_mask(start, end);
+                            let slot_bits = ((value >> num_bits_set) & create_set_width_bit_mask(start, end)) << start;
+                            self.data[byte] |= slot_bits as u8;
+                            num_bits_set += (end - start + 1) as u64;
+
+                            bits_placed += take as usize;
+                            remaining -= take;
+                        }
+                    }
+                });
+                bit_offset = quote! { #bit_offset + #field_bits};
             }
         }
     } else {
-        return syn::Error::new_spanned(&data_struct.fields, "#[bitfield] requires named fields")
+        return syn::Error::new_spanned(&input.fields, "#[bitfield] requires named fields")
             .to_compile_error()
             .into();
     }
@@ -56,6 +103,8 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
                     data: [0u8; #num_bytes]
                 }
             }
+
+            #(#getters_setters)*
         }
     };
     expanded.into()
--- bitfield/src/lib.rs
+++ bitfield/src/lib.rs
@@ -15,45 +15,63 @@ pub mod checks;
 
 pub trait Specifier {
     const BITS: usize;
+    type Ty;
 }
 
 pub struct FixedBits<const N: usize>();
 
-impl<const N: usize> Specifier for FixedBits<N> {
-    const BITS: usize = N;
-}
-
 macro_rules! fixed_bytes_aliases {
-    ($($name:ident<$N:literal>),* $(,)?) => {
+    ($($name:ident<$N:literal, $ty:ty>),* $(,)?) => {
         $(
             pub type $name = FixedBits<$N>;
+            impl Specifier for FixedBits<$N> {
+                const BITS: usize = $N;
+                type Ty = $ty;
+            }
         )*
     };
 }
 
 fixed_bytes_aliases! {
-    B1<1>,
-    B2<2>,
-    B3<3>,
-    B4<4>,
-    B5<5>,
-    B6<6>,
-    B7<7>,
-    B8<8>,
-    B9<9>,
-    B10<10>,
-    B11<11>,
-    B12<12>,
-    B13<13>,
-    B14<14>,
-    B15<15>,
-    B16<16>,
-    B17<17>,
-    B18<18>,
-    B19<19>,
-    B20<20>,
-    B21<21>,
-    B22<22>,
-    B23<23>,
-    B24<24>,
+    B1<1, u8>,
+    B2<2, u8>,
+    B3<3, u8>,
+    B4<4, u8>,
+    B5<5, u8>,
+    B6<6, u8>,
+    B7<7, u8>,
+    B8<8, u8>,
+    B9<9, u16>,
+    B10<10, u16>,
+    B11<11, u16>,
+    B12<12, u16>,
+    B13<13, u16>,
+    B14<14, u16>,
+    B15<15, u16>,
+    B16<16, u16>,
+    B17<17, u32>,
+    B18<18, u32>,
+    B19<19, u32>,
+    B20<20, u32>,
+    B21<21, u32>,
+    B22<22, u32>,
+    B23<23, u32>,
+    B24<24, u32>,
+}
+
+pub fn create_get_bit_mask(start: u8, end: u8) -> u8 {
+    let mut mask: u8 = 0b00000000;
+    for i in start..=end {
+        mask |= 1 << i;
+    }
+    mask
+}
+
+pub fn create_set_width_bit_mask(start: u8, end: u8) -> u64 {
+    debug_assert!(end >= start && (end - start) < 64);
+    let mut mask: u64 = 0b0;
+    for i in 0..=end - start {
+        mask |= 1 << i;
+    }
+    mask
 }
